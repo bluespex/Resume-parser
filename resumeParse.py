51f6d4b89093715f2b651ee1ad54f63ea3b5715b
Piyush <44617356+piyush-shandilya@users.noreply.github.com>
2020-11-21 23:14:48 +0530

    resume mostly pasrsed

--- resumeParse.py
+++ resumeParse.py
@@ -3,10 +3,19 @@ from pdfminer.pdfinterp import PDFPageInterpreter
 from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFSyntaxError
 import io
 import re
+import pandas as pd
+import os
+import constants as cs
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
 
 details = {}
+
+import spacy 
 
 def extract_text_from_pdf(pdf_path):
     with open(pdf_path, 'rb') as fh:
@@ -68,23 +77,199 @@ def extract_roll(roll):
     else:
         return None
 
+def extract_education(nlp_text):
+    '''
+    Helper function to extract education from spacy nlp text
+    :param nlp_text: object of `spacy.tokens.doc.Doc`
+    :return: tuple of education degree and year if year if found
+             else only returns education degree
+    '''
+    edu = {}
+    # Extract education degree
+    try:
+        for index, text in enumerate(nlp_text):
+            for tex in text.split():
+                tex = re.sub(r'[?|$|.|!|,]', r'', tex)
+                if tex.upper() in cs.EDUCATION and tex not in cs.STOPWORDS:
+                    edu[tex] = text + nlp_text[index + 1]
+    except IndexError:
+        pass
+
+    # Extract year
+    education = []
+    for key in edu.keys():
+        year = re.search(re.compile(cs.YEAR), edu[key])
+        if year:
+            education.append((key, ''.join(year.group(0))))
+        else:
+            education.append(key)
+    return education
+
+def extract_experience(resume_text):
+    '''
+    Helper function to extract experience from resume text
+    :param resume_text: Plain resume text
+    :return: list of experience
+    '''
+    wordnet_lemmatizer = WordNetLemmatizer()
+    stop_words = set(stopwords.words('english'))
+
+    # word tokenization
+    word_tokens = nltk.word_tokenize(resume_text)
+
+    # remove stop words and lemmatize
+    filtered_sentence = [
+            w for w in word_tokens if w not
+            in stop_words and wordnet_lemmatizer.lemmatize(w)
+            not in stop_words
+        ]
+    sent = nltk.pos_tag(filtered_sentence)
+
+    # parse regex
+    cp = nltk.RegexpParser('P: {<NNP>+}')
+    cs = cp.parse(sent)
+
+    # for i in cs.subtrees(filter=lambda x: x.label() == 'P'):
+    #     print(i)
+
+    test = []
+
+    for vp in list(
+        cs.subtrees(filter=lambda x: x.label() == 'P')
+    ):
+        test.append(" ".join([
+            i[0] for i in vp.leaves()
+            if len(vp.leaves()) >= 2])
+        )
+
+    # Search the word 'experience' in the chunk and
+    # then print out the text after it
+    x = [
+        x[x.lower().index('experience') + 10:]
+        for i, x in enumerate(test)
+        if x and 'experience' in x.lower()
+    ]
+    return x
+
+
+def extract_skills(nlp_text, noun_chunks, skills_file=None):
+    '''
+    Helper function to extract skills from spacy nlp text
+    :param nlp_text: object of `spacy.tokens.doc.Doc`
+    :param noun_chunks: noun chunks extracted from nlp text
+    :return: list of skills extracted
+    '''
+    tokens = [token.text for token in nlp_text if not token.is_stop]
+    if not skills_file:
+        data = pd.read_csv(
+            os.path.join(os.path.dirname(__file__), 'skills.csv')
+        )
+    else:
+        data = pd.read_csv(skills_file)
+    skills = list(data.columns.values)
+    skillset = []
+    # check for one-grams
+    for token in tokens:
+        if token.lower() in skills:
+            skillset.append(token)
+
+    # check for bi-grams and tri-grams
+    for token in noun_chunks:
+        token = token.text.lower().strip()
+        if token in skills:
+            skillset.append(token)
+    return [i.capitalize() for i in set([i.lower() for i in skillset])]
+
+
+def extract_entity_sections_grad(text):
+    '''
+    Helper function to extract all the raw text from sections of
+    resume specifically for graduates and undergraduates
+    :param text: Raw text of resume
+    :return: dictionary of entities
+    '''
+    text_split = [i.strip() for i in text.split('\n')]
+    # sections_in_resume = [i for i in text_split if i.lower() in sections]
+    entities = {}
+    key = False
+    for phrase in text_split:
+        if len(phrase) == 1:
+            p_key = phrase
+        else:
+            p_key = set(phrase.lower().split()) & set(cs.RESUME_SECTIONS_GRAD)
+        try:
+            p_key = list(p_key)[0]
+        except IndexError:
+            pass
+        if p_key in cs.RESUME_SECTIONS_GRAD:
+            entities[p_key] = []
+            key = p_key
+        elif key and phrase.strip():
+            entities[key].append(phrase)
+
+    # entity_key = False
+    # for entity in entities.keys():
+    #     sub_entities = {}
+    #     for entry in entities[entity]:
+    #         if u'\u2022' not in entry:
+    #             sub_entities[entry] = []
+    #             entity_key = entry
+    #         elif entity_key:
+    #             sub_entities[entity_key].append(entry)
+    #     entities[entity] = sub_entities
+
+    # pprint.pprint(entities)
+
+    # make entities that are not found None
+    # for entity in cs.RESUME_SECTIONS:
+    #     if entity not in entities.keys():
+    #         entities[entity] = None
+    return entities
+
+
 
 def fun(pp):
     resume_path = pp
     text_file = ""
     for page in extract_text_from_pdf(resume_path):
         text_file += ' ' + page
-    
+
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(text_file)
+    __noun_chunks = list(doc.noun_chunks)
+    # __skills_file = 1
+    skills = extract_skills(
+                    doc,
+                    __noun_chunks
+                )
+    entities = extract_entity_sections_grad(text_file)
+    # print(entities)
+    experience = extract_experience(text_file)
+    # print(experience)
+
+
     text = text_file.split(' ')
     resume_text = []
     for i in text:
         if i is not '':
             resume_text.append(i)
+
+    
     details["Name"] = resume_text[0] + ' ' + resume_text[1]
     details["RollNo"] = extract_roll(text_file)
     details["Email"] = extract_email(text_file)
     details["Mobile"] = extract_mobile_number(text_file)
-    # print(details)
+    details["skills"] = skills
+    details["experience"] = experience
+    details["entities"] = entities
     return details
 
-# fun("C:/Users/piyush/Desktop/resume-parser/sampleResumes/piyush.pdf")
+    # details["Name"] = resume_text[0] + ' ' + resume_text[1]
+    # details["RollNo"] = extract_roll(text_file)
+    # details["Email"] = extract_email(text_file)
+    # details["Mobile"] = extract_mobile_number(text_file)
+    # # print(details)
+    # return details
+
+# gg = fun("C:/Users/piyush/Desktop/projects/resume-parser/sampleResumes/Apaar kamal.pdf")
+# print(gg)
--- utils.py
+++ utils.py
@@ -1,2 +1,2 @@
-piyush = 'C:/Users/piyush/Desktop/Resume-Parser/sampleResumes'
+piyush = 'C:/Users/piyush/Desktop/projects/Resume-Parser/sampleResumes'
 prajjwal = "D:/SYSTEM DATA/Desktop/Resume-Parser/sampleResumes"
